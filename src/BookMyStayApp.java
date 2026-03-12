d337291b8505086456ce2afa7ecfecd81d436c8b
p-pranjal <pranjaljadaun@gmail.com>
2026-03-12 09:16:29 +0530

    UC1: Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,10 @@
 public class BookMyStayApp {
+        public static void main(String[] args) {
 
-}
+            System.out.println("Welcome to the Hotel Booking Management System");
+            System.out.println("Book My Stay Application v1.0");
+            System.out.println("System initialized successfully.");
+
+        }
+    }
+
